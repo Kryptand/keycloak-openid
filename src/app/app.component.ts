909361e731024d527a0c7636da27096475249c91
Kryptand <goetze-a@outlook.com>
2020-02-03 21:24:37 +0100

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AuthConfig, OAuthService} from "angular-oauth2-oidc";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'oauth';
+  constructor(private oauthService: OAuthService) {
+    const config = new AuthConfig();
+    config.skipIssuerCheck = true;
+    config.issuer = 'http://localhost:8080';
+    config.clientId = 'angular-app-1';
+    config.redirectUri = window.location.origin + '/index.html';
+    config.scope = 'openid profile email';
+
+    this.oauthService.configure(config);
+    this.oauthService.loadDiscoveryDocument('http://localhost:8080/auth/realms/kryptand/.well-known/openid-configuration').then((doc) => {
+      // Eventuelle Tokens aus Url entnehmen
+      this.oauthService.tryLogin({});
+    });
+  }
+
 }
